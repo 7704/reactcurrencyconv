eea9128df3f478fe6b04f26592d2f17d5605a7f1
Shubham Kumar <shu7704@gmail.com>
2024-06-26 17:10:20 +0530

    first commit

---
+++ src/hooks/useCurr.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+function useCurr(curr) {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${curr}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[curr]));
+    // console.log(data);
+  }, [curr]);
+  // console.log(data);
+  return data;
+}
+
+export default useCurr;
